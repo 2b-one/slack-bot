be372467667263b3bd96e3caaaa8535d2bc5054e
Dmitrii Samsonov <dmitrii.samsonov@ataccama.com>
2020-04-18 20:27:56 +0200

    chore: add linter

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,23 +7,28 @@ const Api = Router()
 
 Api.use(clientVerification)
 Api.post('/commands', async (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/camelcase
   const { team_id, user_id, command, text } = req.body as SlackCommand
   switch (command) {
-    case '/pingme':
+    case '/pingme': {
       app.subscribe(team_id, user_id, 'fe', text)
       res.status(200).send('done.')
       return
+    }
 
-    case '/mypings':
+    case '/mypings': {
       const subs = app.getSubscriptions(team_id, user_id)
       res
         .status(200)
         .send(subs.map(sub => `${sub.projectId.toLocaleUpperCase()} ${sub.branchName}`).join('\n'))
 
       return
+    }
 
-    default:
+    default: {
       res.status(200).send('Command not found.')
+      return
+    }
   }
 })
 
