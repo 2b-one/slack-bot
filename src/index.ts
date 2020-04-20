b6b8438cf9fc5775be571e18481b96b703a280b4
Dmitrii Samsonov <dmitrii.samsonov@ataccama.com>
2020-04-20 15:35:57 +0200

    feat: self reanimation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser'
 import { config } from 'dotenv'
 import express from 'express'
+import https from 'https'
 import { apiController } from './api'
 
 config()
@@ -12,4 +13,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/api', apiController)
+
+app.get('/status', (req, res) => {
+  res.status(200).send('OK')
+})
+
 app.listen(port)
+
+const statusUrl = process.env.bot_status
+if (statusUrl) {
+  setInterval(() => {
+    https.get(statusUrl)
+  }, 1000 * 60 * 25)
+} else {
+  console.warn('"bot_status" variable isn\'t set')
+}
