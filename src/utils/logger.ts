9a4c4e43713806338197e9d6028c6c568b6308cb
Dmitrii Samsonov <dmitrii.samsonov@ataccama.com>
2020-05-03 16:33:04 +0200

    chore: remove file storage for logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,4 @@
 import { createLogger, format, transports } from 'winston'
-import 'winston-daily-rotate-file'
 
 const logger = createLogger({
   level: 'info',
@@ -13,27 +12,10 @@ const logger = createLogger({
   ),
   defaultMeta: { service: '2b-slack-bot' },
   transports: [
-    new transports.DailyRotateFile({
-      filename: 'error.%DATE%.log',
-      dirname: 'logs',
-      level: 'error',
-      zippedArchive: true,
-    }),
-    new transports.DailyRotateFile({
-      filename: 'info.%DATE%.log',
-      dirname: 'logs',
-      level: 'info',
-      zippedArchive: true,
-    }),
-  ],
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
     new transports.Console({
       format: format.combine(format.colorize(), format.simple()),
     }),
-  )
-}
+  ],
+})
 
 export { logger }
