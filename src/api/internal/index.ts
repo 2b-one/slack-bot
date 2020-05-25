1776408bd45744cf1ed5f1ed36b3db395854822d
Dmitrii Samsonov <dmitrii.samsonov@ataccama.com>
2020-05-25 20:45:26 +0200

    chore: remove unused controller

--- src/api/internal/index.ts
+++ src/api/internal/index.ts
@@ -1,13 +1,11 @@
 import bodyParser from 'body-parser'
 import { Router } from 'express'
-import { bitbucketController } from './bitbucketController'
 import { bitbucketHookController } from './bitbucketHookController'
 import { jenkinsController } from './jenkinsController'
 
 const internalController = Router()
 
 internalController.use(bodyParser.json())
-internalController.use(bitbucketController)
 internalController.use(bitbucketHookController)
 internalController.use(jenkinsController)
 
